07d97001124d69266e64cd9d9a9e86530dd4b0d7
yokinist <ikd618@gmail.com>
2025-01-29 15:17:28 +0900

    refactor: Update Opportunity type with new 'type' field

--- types/index.ts
+++ types/index.ts
@@ -1,85 +1,86 @@
 export type User = {
-  id: string
-  name: string
-  email: string
-  role: "visitor" | "leader"
-  createdAt: Date
-  updatedAt: Date
-}
+  id: string;
+  name: string;
+  email: string;
+  role: "visitor" | "leader";
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 export type Project = {
-  id: string
-  title: string
-  description: string
-  categories: Category[]
-  skills: Skill[]
-  createdAt: Date
-  updatedAt: Date
-  opportunities: Opportunity[]
-  icon: string
-}
+  id: string;
+  title: string;
+  description: string;
+  categories: Category[];
+  skills: Skill[];
+  createdAt: Date;
+  updatedAt: Date;
+  opportunities: Opportunity[];
+  icon: string;
+};
 
 export type Opportunity = {
-  id: string
-  title: string
-  description: string
-  status: "open" | "in_progress" | "closed"
-  projectId: string
-  hostId: string
-  startsAt: string
-  endsAt: string
-  categories: Category[]
-  skills: Skill[]
-  createdAt: Date
-  updatedAt: Date
+  id: string;
+  title: string;
+  description: string;
+  type: 'EVENT' | 'QUEST';
+  status: "open" | "in_progress" | "closed";
+  projectId: string;
+  hostId: string;
+  startsAt: string;
+  endsAt: string;
+  categories: Category[];
+  skills: Skill[];
+  createdAt: Date;
+  updatedAt: Date;
   host: {
-    name: string
-    image: string
-  }
-  image: string
+    name: string;
+    image: string;
+  };
+  image: string;
   project?: {
-    title: string
-    description: string
-    icon: string
-  }
+    title: string;
+    description: string;
+    icon: string;
+  };
   relatedArticles?: {
-    id: string
-    title: string
-    description: string
-    image: string
-    url: string
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    url: string;
     source: {
-      name: string
-      icon: string
-    }
-  }[]
-}
+      name: string;
+      icon: string;
+    };
+  }[];
+};
 
 export type Skill = {
-  id: string
-  name: string
-  description: string
-}
+  id: string;
+  name: string;
+  description: string;
+};
 
 export type Category = {
-  id: string
-  name: string
-  description: string
-}
+  id: string;
+  name: string;
+  description: string;
+};
 
 export type Message = {
-  id: string
-  senderId: string
-  receiverId: string
-  content: string
-  createdAt: Date
-  read: boolean
-}
+  id: string;
+  senderId: string;
+  receiverId: string;
+  content: string;
+  createdAt: Date;
+  read: boolean;
+};
 
 export type Notification = {
-  id: string
-  userId: string
-  content: string
-  read: boolean
-  createdAt: Date
-}
+  id: string;
+  userId: string;
+  content: string;
+  read: boolean;
+  createdAt: Date;
+};
